666b28d46caf01f58c623bdefba7142e601fafb6
sonu <srssoumyaranjansahu123@gmail.com>
2024-02-27 15:27:32 +0530

    server

--- src/utils/serverurl.js
+++ src/utils/serverurl.js
@@ -1,2 +1,2 @@
-//export const URL="https://ecommerce-backend-project.onrender.com/app/"
-export const URL="http://localhost:8001/app/"
+export const URL="https://ecommerce-backend-project.onrender.com/app/"
+//export const URL="http://localhost:8001/app/"
